8db0c8e03767956d3333b83189b8f5c35d8e7732
Elena <hellenatsvetkova@gmail.com>
2025-03-27 08:23:21 +0200

    fix: calendar model; feat: created article model

--- secretSpark/notes/models.py
+++ secretSpark/notes/models.py
@@ -21,23 +21,12 @@ class CyclePhase(models.Model):
 
 class CycleCalendar(models.Model):
 
-    # user = models.OneToOneField(
-    #     to='users.User',
-    #     on_delete=models.CASCADE
-    # )
     start_date = models.DateField()
     cycle_length = models.SmallIntegerField() # Varying from 28 to 35 days
     period_length = models.SmallIntegerField() # Varying from 3 to 7 days
     last_period_date = models.DateField()
 
-class CyclePhaseOccurrence(models.Model):
-    calendar = models.ForeignKey(CycleCalendar, on_delete=models.CASCADE)  # One calendar has multiple phase occurrences
-    phase = models.ForeignKey(CyclePhase, on_delete=models.CASCADE)  # Links to a specific phase
-    start_date = models.DateField()
-    end_date = models.DateField()
 
-#     def __str__(self):
-#         return f"{self.calendar.user.username}'s {self.phase.name} Phase ({self.start_date} - {self.end_date})"
 # #
 # class Diary(models.Model):
 #
