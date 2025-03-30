6208ebffc92b606b259394ca4e96ef2558d89d80
Elena <hellenatsvetkova@gmail.com>
2025-03-30 10:16:27 +0300

    feat: created article forms

---
+++ secretSpark/articles/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from secretSpark.articles.models import Article
+
+
+class ArticleBaseForm(forms.ModelForm):
+
+    class Meta:
+        model = Article
+        fields = "__all__"
+
+
+class ArticleCreateForm(ArticleBaseForm):
+    pass
+
+class ArticleEditForm(ArticleBaseForm):
+    pass
+
+class ArticleDeleteForm(ArticleBaseForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].disabled = True
+
+
+
+
+
--- secretSpark/articles/views.py
+++ secretSpark/articles/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from secretSpark.articles.forms import ArticleCreateForm
+
+
+def create_article(request):
+
+    form = ArticleCreateForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+
+
+
+
+
+
+
