cb5b29282c084f6d4d0999e415d9ae6bd5e2709d
Elena <hellenatsvetkova@gmail.com>
2025-03-31 19:43:21 +0300

    create urls and views for CRUD of article model

--- secretSpark/articles/urls.py
+++ secretSpark/articles/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 
-from secretSpark.articles.views import create_article
+from secretSpark.articles.views import create_article, edit_article, delete_article, details_article
 
 urlpatterns = [
     path('create-article/', create_article, name='create-article'),
+    path('<int:pk>', include([
+        path('edit-article', edit_article, name='edit-article'),
+        path('delete-article', delete_article, name='delete-article'),
+        path('details-article', details_article, name='details-article'),
+    ]))
 ]
--- secretSpark/articles/views.py
+++ secretSpark/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from secretSpark.articles.forms import ArticleCreateForm
+from secretSpark.articles.models import Article
 
 
 def create_article(request):
@@ -14,9 +15,21 @@ def create_article(request):
     context = {
         "form": form,
     }
-    return render(request, 'articles/create-article.html', context)
+    return render(request, template_name='articles/create-article.html', context=context)
 
 
+def edit_article(request, pk: int):
+    pass
 
 
+def delete_article(request, pk: int):
+    ...
+
+def details_article(request, pk: int):
+    article = Article.objects.get(pk=pk)
+    context = {
+        "article": article
+    }
+
+    return render(request, template_name='articles/details-article.html', context=context)
 
