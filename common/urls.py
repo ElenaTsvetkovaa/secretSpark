37e8bae921b4de9457e0367666d95c515c918e6e
Elena <hellenatsvetkova@gmail.com>
2025-08-02 08:40:13 +0300

    add check for health check of Railway container

--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from common.views import index
+from common.views import index, health_check
 
 urlpatterns = [
     path('', index, name='home'),
+    path('health/', health_check, name='health_check'),
 ]
