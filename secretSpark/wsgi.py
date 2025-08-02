a2fc33ee10db6e7810baaa799a739925b9dc92af
Elena <hellenatsvetkova@gmail.com>
2025-08-02 09:32:35 +0300

    fix: removed unsed whitenoise import

--- secretSpark/wsgi.py
+++ secretSpark/wsgi.py
@@ -7,10 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 import os
-from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secretSpark.settings')
 
 application = get_wsgi_application()
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
